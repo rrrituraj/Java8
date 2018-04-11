9e30b39a1f4d68bbc0c868390fd7688fd4df568b
Rituraj <guptamaharaj@gmail.com>
2018-04-11 23:48:37 +0530

    Java8 Stream API
    Signed-off-by: Rituraj <guptamaharaj@gmail.com>

--- src/main/java/Java8/Java8_Stream/StreamBuilder.java
+++ src/main/java/Java8/Java8_Stream/StreamBuilder.java
@@ -7,5 +7,7 @@ public class StreamBuilder {
 		Stream<String> build = Stream.<String>builder().add("a").add("v").add("c").build();
 		build.forEach(System.out::println);
 		System.out.println(build);
+
+
 	}
 }
