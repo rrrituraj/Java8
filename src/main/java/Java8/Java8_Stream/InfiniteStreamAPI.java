44f76a1e5c4a37d81c18fca2a412990e2f8509db
Rituraj <guptamaharaj@gmail.com>
2018-04-12 22:13:56 +0530

    name change
    Signed-off-by: Rituraj <guptamaharaj@gmail.com>

--- src/main/java/Java8/Java8_Stream/InfiniteStreamAPI.java
+++ src/main/java/Java8/Java8_Stream/InfiniteStreamAPI.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class IfiniteStreamAPI {
+public class InfiniteStreamAPI {
 	public static void main(String[] args) {
 		Stream<Integer> infiniteInteger = Stream.iterate(2, i -> i + 2);
 		List<Integer> list = infiniteInteger.limit(10).collect(Collectors.toList());
